da8bf173db7bc22d039e49cabc57c4573775b326
Abderahmane ALIOUAT <ab.aliouat@gmail.com>
2026-08-09 23:31:21 +0200

    Ajout du projet Historiya

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,10 @@
-export default function Home() {
+import React from 'react'
+
+const page = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="space-y-3 max-w-3xl">
-        <h1 className="text-5xl font-semibold">Home page</h1>
+        <h1 className="text-5xl font-semibold">About page</h1>
         <p className="text-gray-400">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
           Beatae, enim atque, dolorem tempora reiciendis iure unde quod 
@@ -12,5 +14,7 @@ export default function Home() {
         </p>
       </div>
     </div>
-  );
+  )
 }
+
+export default page
