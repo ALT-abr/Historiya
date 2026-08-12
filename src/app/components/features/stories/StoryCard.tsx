f765bb11160f8167448ce2e225d82574bdd26052
Abderahmane ALIOUAT <ab.aliouat@gmail.com>
2026-08-12 19:26:23 +0200

    ajout de la cart des stories

---
+++ src/app/components/features/stories/StoryCard.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import Image from "next/image";
+
+type StoryCardProps = {
+    title : string;
+    imageSrc : string;
+    imageAlt : string;
+    href : string;
+}
+
+export default function StoryCard({
+    title,
+    imageSrc,
+    imageAlt,
+    href,
+}:StoryCardProps) {
+    return(
+        <Link 
+          href={href}
+          className="
+           group block w-full max-w-[410px]
+           overflow-hidden rounded-[55px]
+           bg-[#fff1dc]
+           shadow-[0_15px_25px_rgba(0,0,0,0.22)]
+           transition duration-300
+           hover:-translate-y-2
+           hover:shadow-[0_22px_35px_rgba(0,0,0,0.28)]">
+            <div className="relative aspect-[4/4.3] w-full overflow-hidden">
+                <Image 
+                  src={imageSrc}
+                  alt={imageAlt}
+                  fill
+                  sizes="(max-width: 640px) 90vw, 410px"
+                  className="object-cover transition duration-500 group-hover:scale-105"/>
+            </div>
+
+            <div className="flex min-h-[110px] items-center justify-center px-6 py-5">
+                <h3 className="text-center text-3xl font-bold text-black">
+                    {title}
+                </h3>
+            </div>
+         </Link>
+    )
+}
