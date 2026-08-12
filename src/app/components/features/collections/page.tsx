cb554d3c8576f7a6b1559abec0bb73bccb891e51
Abderahmane ALIOUAT <ab.aliouat@gmail.com>
2026-08-12 17:56:34 +0200

    ajout de la cart collection

---
+++ src/app/components/features/collections/page.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image"
+import Link from "next/link"
+
+type CollectionCardProps = {
+    imageSrc : string;
+    imageAlt : string;
+    href : string;
+}
+
+export default function CollectionCard({
+    imageSrc,
+    imageAlt,
+    href,
+}:CollectionCardProps) {
+    return(
+        <Link href={href}>
+            <div 
+              className="
+               relative has-[200px] w-[280px]
+               overflow-hidden rounded-[30px]
+               transition duration-300
+               hover:-translate-y-2
+               hover:shadow-[0_18px_35px_rgba(23,43,88,0.3)]">
+                <Image 
+                  src={imageSrc}
+                  alt={imageAlt}
+                  fill
+                  sizes="280px"
+                  className="object-cover"/>
+            </div>
+        </Link>
+    )
+}
